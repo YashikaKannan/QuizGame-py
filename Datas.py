25b49ef05d9b2be5734f31bf06107333a803bbb4
Yashika Kannan <yashikakannan05@gmail.com>
2024-06-24 22:13:56 +0530

    Datas.py

---
+++ Datas.py
@@ -0,0 +1,27 @@
+data = [
+    {
+        "category": "Science: Computer", "type": "boolean", "difficulty": "medium",
+        "question": " A stack is a data structure used to implement a LIFO (Last In, First Out) mechanism. ",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computer", "type": "boolean", "difficulty": "medium",
+        "question": "The time complexity of the binary search algorithm in a sorted array is O(n). ",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computer", "type": "boolean", "difficulty": "medium",
+        "question": "SQL stands for Simple Query Language and is primarily used for managing and manipulating NoSQL databases.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computer", "type": "boolean", "difficulty": "medium",
+        "question": "In blockchain technology, a block can be altered after it has been added to the blockchain without affecting the integrity of the chain.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computer", "type": "boolean", "difficulty": "easy",
+        "question": "POP3 is a protocol commonly used to retrieve email messages from a mail server. ",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    }
+]
